320f45aa67068b6e05c55b87ca28c2990fe00dbf
SolidRules <amikheev1993@gmail.comm>
2022-08-03 16:14:52 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-let storage = [];
-let storage1 = [];
-const OPEN_BRACKETS = ['(','{','[','|','1','3','5','7','8'];
-const BRACKET_PAIRS = {
-  ')': '(',
-  '}': '{',
-  ']': '[',
-  '|': '|',
-  '2': '1',
-  '4': '3',
-  '6': '5',
-  '7': '7',
-  '8': '8',
-};
+let stack = [];
+let openBr = [];
+let pairsBr = {};
+
+for (let i = 0; i < bracketsConfig.length; i++) {
+  openBr[i] = bracketsConfig[i][0];
+  pairsBr[bracketsConfig[i][1]] = openBr[i];
+}
 
 for (let i=0; i<str.length; i++) {
   let currentSymbol = str[i];
-  if(OPEN_BRACKETS.includes(currentSymbol)) {
-    storage.push(currentSymbol);
-    if((currentSymbol === '|') || (currentSymbol === '7') || (currentSymbol === '8')) storage1.push(currentSymbol);
-  } else {
-    if(storage.length === 0) {
-      return false;
-    } else {
-    let g = storage.length-1;
-    let j = g;
-    let topElements = storage[j];    
-    while((topElements === '|') || (topElements === '7') || (topElements === '8')) {
-      j--;
-      topElements = storage[j]
-    } if((g-j) % 2) {
-      return false }
-      { if(topElements === BRACKET_PAIRS[currentSymbol]) {storage.splice(j, 1)} else {return false}
-    }
-  }}
+  let topElement = stack[stack.length - 1];
+  if((stack.length === 0) || (topElement !== pairsBr[currentSymbol])) {
+    stack.push(currentSymbol);
+  } else { 
+    stack.pop();
   }
-  let filteredStorage = storage.filter(element => element !== '|').filter(element => element !== '7').filter(element => element !== '8');
-return  `storage=${storage}=storage1=${storage1}=filteredStorage=${filteredStorage}=`/* (filteredStorage.length === 0) && (storage1.length % 2 === 0); */
+}
+return stack.length === 0;
 }
